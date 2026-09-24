356ebf06a8c7af7841a57ff7b31e8ce187b2f3fe
Victor Rodriguez <kuchox@pm.me>
2026-09-24 11:49:45 -0500

    Render invoice PDFs through the new pipeline

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,11 @@ class Invoice < ApplicationRecord
   has_many :line_items
 
   def pdf
-    LegacyPdfRenderer.new(self).render
+    if Flipper.enabled?(:invoice_pdf_v2)
+      InvoicePdf::Pipeline.new(self).render
+    else
+      LegacyPdfRenderer.new(self).render
+    end
   end
 
   def preview_lines
